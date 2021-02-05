ce797f15254fcec2d5495c9683b2bb841fdb3def
antipenko-nastya <78617573+antipenko-nastya@users.noreply.github.com>
2021-02-05 22:29:14 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+var btn1 = document.querySelector('#id1')
+btn1.addEventListener('click',() => {
+    window.location.href = ['http://mgke.minsk.edu.by/']
+})
+var btn2 = document.querySelector('#id2')
+btn2.addEventListener('click',() => {
+    document.body.style.background = '#FFCCFF'
+})
